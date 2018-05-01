adcdd5ee942ce0dd3981c77c500d20f50434bf4e
Pamela Frieze <pfrieze@gmail.com>
2018-05-01 21:17:16 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -32,7 +32,7 @@ class Transfer #space for transaction between two bank accounts. is able to reje
       sender.balance += amount
       @status = "reversed"
     else
-      reject_transfer
+      @status = "rejected"
     end
   end
 
