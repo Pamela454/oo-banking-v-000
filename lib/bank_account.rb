159ef2c9db0dbb9fcaca710932bf401c0e0011fb
Pamela Frieze <pfrieze@gmail.com>
2018-05-01 19:57:40 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,28 +1,3 @@
-#bank account - money can be transferred between instances by a Transfer class.
 class BankAccount
-  attr_reader :name
-  attr_accessor :balance, :status
-
-  def initialize(name)
-    @name = name
-    @balance = 1000
-    @status = "open"
-  end
-
-  def deposit(money)
-    self.balance += money
-  end
-
-  def display_balance
-    "Your balance is $#{@balance}."
-  end
-
-  def valid?
-    balance > 0 && status == "open"
-  end
-
-  def close_account
-    self.status = "closed"
-  end
 
 end
