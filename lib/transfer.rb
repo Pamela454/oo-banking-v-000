af8cea35ca3f48ad809b142f1a454f2fd64291be
Pamela Frieze <pfrieze@gmail.com>
2018-05-01 20:36:29 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,11 +1,12 @@
 require 'pry'
 class Transfer #space for transaction between two bank accounts. is able to reject a transfer.
   # your code here
-  attr_reader :sender, :receiver, :amount 
+  attr_reader :sender, :receiver, :amount
 
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
     @amount = amount
+    @status = "pending"
   end
 end
