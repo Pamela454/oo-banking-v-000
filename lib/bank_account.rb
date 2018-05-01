18a1e18979b386f58e9e00359b35e4f57eeaa62f
Pamela Frieze <pfrieze@gmail.com>
2018-05-01 20:12:49 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,5 +1,5 @@
 class BankAccount #one instance can transfer money to another instance.
-  attr_accessor :name, :balance, :status
+  attr_accessor :balance, :status
   attr_reader :name
 
   def initialize(name)
